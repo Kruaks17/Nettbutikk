a75bf7f7c13462918511e19b4745694097e22577
Kruaks17 <aksel.kj@hotmail.com>
2021-10-15 13:27:44 +0200

    commit

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,95 +1,94 @@
-import {range} from "./products.js";
+import { range } from "./products.js";
 
 let pruductPageGrid = document.querySelector(".grid");
 
 //legge til prdukter til siden og synelig
-for (let i=0; i < range.length; i++){
+for (let i = 0; i < range.length; i++) {
 
-let product =document.createElement ("div");
-pruductPageGrid.appendChild(product);
+    let product = document.createElement("div");
+    pruductPageGrid.appendChild(product);
 
-let img = document.createElement("img");
-img.className = "product-img";
-img.src = range[i].image;
-product.appendChild(img);
+    let img = document.createElement("img");
+    img.className = "product-img";
+    img.src = range[i].image;
+    product.appendChild(img);
 
-let h1= document.createElement ("h1");
-h1.className = "product-name";
-h1.innerHTML = range [i].name;
-product.appendChild(h1);
+    let h1 = document.createElement("h1");
+    h1.className = "product-name";
+    h1.innerHTML = range[i].name;
+    product.appendChild(h1);
 
-let p= document.createElement ("p");
-p.className = "product-price";
-p.innerHTML = "$" + range [i].price;
-product.appendChild(p);
+    let p = document.createElement("p");
+    p.className = "product-price";
+    p.innerHTML = "$" + range[i].price;
+    product.appendChild(p);
 
-let button= document.createElement ("button");
-button.className = "add-to-cart";
-button.innerHTML = "Add to cart";
-product.appendChild(button);
-button.id = range[i].id
+    let button = document.createElement("button");
+    button.className = "add-to-cart";
+    button.innerHTML = "Add to cart";
+    product.appendChild(button);
+    button.id = range[i].id
 };
-
 //cart
 let cartContainer = document.querySelector("#cart");
-let shoppingCart =[];
+let shoppingCart = [];
 
-function addToArray (e){
+function addToArray(e) {
 
-    for (let i = 0; i < range.length; i++){
+    for (let i = 0; i < range.length; i++) {
 
-        if (range[i].id === e.target.id){
+        if (range[i].id === e.target.id) {
             shoppingCart.push(range[i]);
         }
     };
 
-//legge till items til cart og gjøres synlig under cart
-addToCart();
+    //legge till items til cart og gjøres synlig under cart
+    addToCart();
 }
-function addToCart(){
+function addToCart() {
 
     let totalPrice = 0;
 
-    cartContainer.innerHTML="";
+    cartContainer.innerHTML = "";
 
-    shoppingCart.forEach((e,index) =>{
+    shoppingCart.forEach((e, index) => {
 
         let cartColumn1 = document.createElement("div");
-        cartColumn1.className ="cart-collumn1";
+        cartColumn1.className = "cart-collumn1";
         cartContainer.appendChild(cartColumn1);
 
         let cartColumn2 = document.createElement("div");
-        cartColumn2.className= "cart-column2";
+        cartColumn2.className = "cart-column2";
         cartContainer.appendChild(cartColumn2);
 
         let cartProductName = document.createElement("div");
-        cartProductName.className="cart-name";
-        cartProductName.innerHTML=e.name;
+        cartProductName.className = "cart-name";
+        cartProductName.innerHTML = e.name;
         cartColumn2.appendChild(cartProductName);
-    
+
         let cartProductPrice = document.createElement("p");
-        cartProductPrice.className="cart-price";
-        cartProductPrice.innerHTML= "$" + e.price;
+        cartProductPrice.className = "cart-price";
+        cartProductPrice.innerHTML = "$" + e.price;
         cartColumn2.appendChild(cartProductPrice);
 
-        let removeBtn= document.createElement ("button");
+        let removeBtn = document.createElement("button");
         removeBtn.className = "removebtn";
         removeBtn.dataset.remove = index;
         removeBtn.innerHTML = "X";
-        cartContainer.appendChild(removeBtn); 
-        
+        cartContainer.appendChild(removeBtn);
+
 
         removeBtn.addEventListener('click', removeItem);
-        
-        
-        totalPrice += e.price 
-        
+
+
+        totalPrice += e.price
+
     });
-        //viser total prisen for produkter
-        let cartTotalPrice = document.createElement("p");
-        cartTotalPrice.className="total-price";
-        cartTotalPrice.innerHTML= "Total" + "=" + "$" + totalPrice;
-        cartContainer.appendChild(cartTotalPrice);
+    //viser total prisen for produkter
+    let cartTotalPrice = document.createElement("p");
+    cartTotalPrice.className = "total-price";
+    cartTotalPrice.innerHTML = "Total" + "=" + "$" + totalPrice;
+    cartContainer.appendChild(cartTotalPrice);
 
 }
 //totalsum 
@@ -102,20 +101,20 @@ let cartBtn = document.getElementById("cartbtn");
 
 function showCart() {
     if (cartContainer.style.display === "none") {
-      cartContainer.style.display = "block";
+        cartContainer.style.display = "block";
     } else {
-      cartContainer.style.display = "none";
-    } 
-  };
+        cartContainer.style.display = "none";
+    }
+};
 
 cartBtn.addEventListener('click', showCart);
- 
-//fjerne items - en av gangen 
-function removeItem(item) { 
 
-    shoppingCart.splice(item.target.dataset.remove,1)
+//fjerne items - en av gangen 
+function removeItem(item) {
+
+    shoppingCart.splice(item.target.dataset.remove, 1)
     addToCart()
-    
+
     console.log(item.target.dataset.remove);
 }
 
